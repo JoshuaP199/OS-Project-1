9f6e4ab4f8bf305abbd36b0d8ac51d3c21dcaf8d
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-07 19:29:21 -0500

    Add files via upload
    This is to show how the program works

---
+++ understand.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum; //Global
+
+void *runner(void *param); //want to understand the reason for pointers here
+
+void *runner(void *param) {
+    printf("param %s = upper\n", param); //param is argv[]
+    int i, upper = atoi(param); //atoi sets the string of param into an int (atoi sets strings to ints)
+    sum = 0;
+    if(upper > 0) {
+        for(i = 1; i <= upper; i++){
+            sum += i;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t tid;
+    pthread_attr_t attr;
+    printf("argc %d\n", argc);      // argc reads the positions
+    printf("argv[1] %s\n", argv[1]);   //reads the actual input from the user depending on the position starting at [int]
+
+    pthread_attr_init(&attr);    //defaults
+
+    pthread_create(&tid, &attr, runner, argv[1]);   //thread creation runner uses argv[1] as its param
+
+    pthread_join(tid, NULL);    //waits for thread to exit
+
+    printf("sum = %d\n", sum);
+}
