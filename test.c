663660fb2866c62f4ae6ec7dde71eefcd6ca393f
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-09 21:01:54 -0400

    Add files via upload

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 int sum = 0;
 int finalSUM = 0;
 int M = 8, N = 256, lb = 1, ub = 0;
-char a[8]; //check array on Chegg
+char a[8];  //Check array on Chegg
  
 int q, rem;
 
@@ -13,17 +13,16 @@ void main(){
     //printf("q = %d\n", q);        **
     rem = N%M;
     //printf("rem = %d\n", rem);            **
-    for(int i = 1; i < M+1; i++){ //this only works for if N/M is even, need to modify if its not
+    for(int i = 1; i < M+1; i++){   //this only works for if N/M is even, need to modify if its not
         ub = lb +q;
-        //printf("1st ub = %d\n", ub);          **
+        //printf("1st ub = %d\n", ub);             **    
         int cur = lb;
         //printf("1st cur(lb) = %d\n", cur);        **
-
         //printf("ub = %d\n", ub);      **
         sum=0;
-        //create array to hold sums
-        for(int p = 0; p <= q; p++){
-            sum = sum + (lb+p); //IMPORTANt
+
+        for(int p = 0; p <= q; p++){    //This works how it should in terms of the math
+            sum = sum + (lb+p); //IMPORTANT
             //printf("sum = %d\n", sum);
             
         }
@@ -31,6 +30,7 @@ void main(){
         a[0] = 0;
         a[ i ] = sum;
         lb = ub +1;
+        //FOR SOME REASON LAST NUMBER IS WRONG IT GIVES 264 INSTEAD OF 256, MOST LIKELY HAS TO DO WITH LOWER/UPPER BOUNDS OR Q 
     }
     
         for(int g=1; g<M+1; g++){
@@ -43,5 +43,5 @@ void main(){
     
     
     //use the array stated earlier ^ and add those to get the total sum
-    printf("sum of 1 to %d = %d", ub, finalSUM); //replace 1000 with total sum (create total sum variable)
+    printf("sum of 1 to %d = %d", ub, finalSUM);
 }
