07c7159279096161c8b62127ecfc014fa02c1a57
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-10 13:29:09 -0400

    Add files via upload

--- test.c
+++ test.c
@@ -1,41 +1,90 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int sum = 0;
 int finalSUM = 0;
-int M = 8, N = 256, lb = 1, ub = 0;
-char *a[8];  //Check array on Chegg
- 
+int lb = 1, ub = 0; 
 int q, rem;
 
-void main(){
-    q = (N/M)-1;
-    //printf("q = %d\n", q);        **
-    rem = N%M;
-    //printf("rem = %d\n", rem);            **
-    for(int i = 1; i < M+1; i++){   //this only works for if N/M is even, need to modify if its not
-        ub = lb +q;
-        //printf("1st ub = %d\n", ub);             **    
-        int cur = lb;
-        //printf("1st cur(lb) = %d\n", cur);        **
-        //printf("ub = %d\n", ub);      **
-        sum=0;
+void *runner(void *param);
 
-        for(int p = 0; p <= q; p++){    //This works how it should in terms of the math
-            sum = sum + (lb+p); //IMPORTANT
-            //printf("sum = %d\n", sum);
-        }
-        printf("sum of %d to %d = %d\n", cur, ub, sum);
-        a[0] = 0;
-        a[ i ] = sum;
-        lb = ub +1;
-       
-    }
-    for(int g=1; g<M+1; g++){
-        int point = a[g];
-        finalSUM = finalSUM + point;
-    }
-    
-    //use the array stated earlier ^ and add those to get the total sum
-    printf("sum of 1 to %d = %d", ub, finalSUM);
+int main(int argc, char *argv[]){
+   pthread_t tid;
+   pthread_attr_t attr;
+
+   if (argc != 3) {
+      fprintf(stderr,"usage: a.out <integer value>\n");
+      /*exit(1);*/
+      return -1;
+   }
+
+   if (atoi(argv[1]) < 0) {
+      fprintf(stderr,"Argument %d must be non-negative\n",atoi(argv[1]));
+      /*exit(1);*/
+      return -1;
+   }
+   
+   if (atoi(argv[2]) <= 0 ) {
+      fprintf(stderr,"Argument %d must be Non-zero and non-negative\n",atoi(argv[1]));
+      /*exit(1);*/
+      return -1;
+   }
+
+   if ( atoi(argv[2]) > atoi(argv[1]) ) {
+      fprintf(stderr,"Argument %d must be non-negative and N > M \n",atoi(argv[1]));
+      /*exit(1);*/
+      return -1;
+   }
+
+   pthread_attr_init(&attr);
+
+   //create the thread
+   int quotient , remainder , N , M ;
+   N = atoi(argv[1]);
+   M = atoi(argv[2]);
+
+   int a[M];
+
+   //the working code 
+   q = (N/M)-1;      
+   rem = N%M;       
+   for(int i = 1; i < M+1; i++){   
+      pthread_create(&tid,&attr,runner,&a[i]);  //SHOW THIS DOES WORK BUT IT DOES NOTHING AS OF RIGHT NOW
+      ub = lb +q;           
+      int cur = lb; 
+      sum=0;
+
+      for(int p = 0; p <= q; p++){   
+         sum = sum + (lb+p);
+      }
+      printf("sum of %d to %d = %d\n", cur, ub, sum);
+      finalSUM += sum;
+      lb = ub +1;
+         
+   }
+
+   /* now wait for the thread to exit */
+   pthread_join(tid,NULL);
+
+   printf("sum of 1 to %d = %d\n", ub, finalSUM);
 }
+
+void *runner(void *param){       //NEED TO CHANGE THE INPUT VARIABLES FOR THIS TO RUN CORRECTLY
+   int i, upper = atoi(param);
+	
+   if (upper > 0) {
+		for (i = 1; i <= upper; i++){
+			sum += i;
+         printf("sum in runner = %d", sum);
+      }
+   }
+   
+   else{
+      printf("runner skipped\n");
+   }
+   
+	pthread_exit(0);
+}
+
+//NEED TO FIX RUNNER SO IT ACTUALLY WORKS HOW IT IS SUPPOSED TO
