cdf62008c82894f1b35a63c910585a8558be4baa
gcovillo <40395875+gcovillo@users.noreply.github.com>
2020-03-15 20:09:46 -0400

    Update PyFinal.py

--- Python EC/PyFinal.py
+++ Python EC/PyFinal.py
@@ -5,46 +5,41 @@ M = int(input("Number of threads to use: "))
 
 q = (N//M)
 rem = N%M
-holder = 0
-
 
 def runner(X, Y, Z):
     if (i == M-1):
         sum = 0
         for p in range((N-lb)+1):
             sum = sum + (lb+p)
+        print("sum of ", lb, " to ", ub, " = ", sum)
+        holder += sum
+        print(holder)
     else:
         sum = 0
         for p in range(q+1):
             sum = sum + (lb+p)
-    print("sum of ", lb, " to ", ub, " = ", sum)
-    holder += sum
+        print("sum of ", lb, " to ", ub, " = ", sum)
+        holder += sum
+        print(holder)
     return holder
     
-def getBoundaries(i):
-    if i == 0:
-        lb = 1
-        ub = lb + (q-1)
-        print(lb, " ", ub)
-        return lb, ub
-    elif (i == M-1):
-        ub = N
-        lb = (ub - (q+rem))
-        print(lb, " ", ub)
-        return lb, ub
-    else:
-        lb = 1 + (q*i)
-        ub = lb + (q-1)
-        print(lb, " ", ub)
-        return lb, ub
-    
 if __name__ == "__main__": 
-    int(lb)
-    int(ub)
     for i in range(M):
-        getBoundaries(i)
+        if i == 0:
+            lb = 1
+            ub = lb + (q-1)
+            print(lb, " ", ub)
+        elif (i == M-1):
+            ub = N
+            lb = (ub - (q+rem))
+            print(lb, " ", ub)
+        else:
+            lb = 1 + (q*i)
+            ub = lb + (q-1)
+            print(lb, " ", ub)
         a1= threading.Thread(target=runner, args=(lb,ub,i)) 
         a1.start()
         a1.join() 
+        holder += holder
     print('Sum of 1 to ', N, ' = ', holder)
     print("Done!") 
