ae92ad9ec3bb49f2ff94a31c3934c105dd8543ae
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-16 09:33:59 -0400

    Removed comments

--- test.c
+++ test.c
@@ -9,46 +9,34 @@ int c = 1;
 
 void *runner();
 
-int main(int argc, char *argv[]){
+void main(int argc, char *argv[]){
    pthread_t tid;
    pthread_attr_t attr;
 
    pthread_attr_init(&attr);
 
-   //create the thread
    int quotient , remainder , N , M ;
    N = atoi(argv[1]);
    M = atoi(argv[2]);
 
    int a[M];
- //
- //
- //
- //
- //
 
    q = (N/M)-1;      
    rem = N%M;       
 
    for(int i = 1; i < M+1; i++){  
       printf("\n"); 
-      pthread_create(&tid,&attr,runner,&a[i]);  //SOMEHOW THIS DOES WORK BUT IT DOES NOTHING AS OF RIGHT NOW
+      pthread_create(&tid,&attr,runner,&a[i]);
       //finalSUM += sum;
       lb = ub +1;
    }
 
    pthread_join(tid,NULL);
 
-//
-//
-//
-//
-//
-
    printf("\nsum of 1 to %d = %d\n", ub, finalSUM);
 }
 
-void *runner(){    //NEED TO CHANGE THE INPUT VARIABLES FOR THIS TO RUN CORRECTLY
+void *runner(){
 	ub = lb +q;             
    int cur = lb; 
    sum=0;
@@ -56,12 +44,12 @@ void *runner(){    //NEED TO CHANGE THE INPUT VARIABLES FOR THIS TO RUN CORRECTL
    for(int p = 0; p <= q; p++){   
       sum = sum + (lb+p);
    }
-   //printf("cur %d ---- ub %d\n", cur, ub);
-   printf("c = %d --- sum of %d to %d = %d\n", c, cur, ub, sum);
-   c++;
-   finalSUM += sum;
+   printf("");
+   printf("sum of %d to %d = %d\n", cur, ub, sum);
+   
+   //printf("c = %d --- sum of %d to %d = %d\n", c, cur, ub, sum);
+   //c++;
+   finalSUM += sum; //MOST INCONSISTANT
 
 	pthread_exit(0);
 }
-
-//NEED TO FIX RUNNER SO IT ACTUALLY WORKS HOW IT IS SUPPOSED TO
