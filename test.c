0d1f0911ea92f7771e13252e580028d362f00c66
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-16 09:19:13 -0400

    Add files via upload

--- test.c
+++ test.c
@@ -2,40 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum = 0;
-int finalSUM = 0;
+int sum, finalSUM = 0;
 int lb = 1, ub = 0; 
 int q, rem;
+int c = 1;
 
-void *runner(void *param);
+void *runner();
 
 int main(int argc, char *argv[]){
    pthread_t tid;
    pthread_attr_t attr;
-
-   if (argc != 3) {
-      fprintf(stderr,"usage: a.out <integer value>\n");
-      /*exit(1);*/
-      return -1;
-   }
-
-   if (atoi(argv[1]) < 0) {
-      fprintf(stderr,"Argument %d must be non-negative\n",atoi(argv[1]));
-      /*exit(1);*/
-      return -1;
-   }
-   
-   if (atoi(argv[2]) <= 0 ) {
-      fprintf(stderr,"Argument %d must be Non-zero and non-negative\n",atoi(argv[1]));
-      /*exit(1);*/
-      return -1;
-   }
-
-   if ( atoi(argv[2]) > atoi(argv[1]) ) {
-      fprintf(stderr,"Argument %d must be non-negative and N > M \n",atoi(argv[1]));
-      /*exit(1);*/
-      return -1;
-   }
 
    pthread_attr_init(&attr);
 
@@ -45,45 +21,46 @@ int main(int argc, char *argv[]){
    M = atoi(argv[2]);
 
    int a[M];
+ //
+ //
+ //
+ //
+ //
 
-   //the working code 
    q = (N/M)-1;      
    rem = N%M;       
-   for(int i = 1; i < M+1; i++){   
-      pthread_create(&tid,&attr,runner,&a[i]);  //SHOW THIS DOES WORK BUT IT DOES NOTHING AS OF RIGHT NOW
-      ub = lb +q;           
-      int cur = lb; 
-      sum=0;
 
-      for(int p = 0; p <= q; p++){   
-         sum = sum + (lb+p);
-      }
-      printf("sum of %d to %d = %d\n", cur, ub, sum);
-      finalSUM += sum;
+   for(int i = 1; i < M+1; i++){  
+      printf("\n"); 
+      pthread_create(&tid,&attr,runner,&a[i]);  //SOMEHOW THIS DOES WORK BUT IT DOES NOTHING AS OF RIGHT NOW
+      //finalSUM += sum;
       lb = ub +1;
-         
    }
 
-   /* now wait for the thread to exit */
    pthread_join(tid,NULL);
 
-   printf("sum of 1 to %d = %d\n", ub, finalSUM);
+//
+//
+//
+//
+//
+
+   printf("\nsum of 1 to %d = %d\n", ub, finalSUM);
 }
 
-void *runner(void *param){       //NEED TO CHANGE THE INPUT VARIABLES FOR THIS TO RUN CORRECTLY
-   int i, upper = atoi(param);
-	
-   if (upper > 0) {
-		for (i = 1; i <= upper; i++){
-			sum += i;
-         printf("sum in runner = %d", sum);
-      }
+void *runner(){    //NEED TO CHANGE THE INPUT VARIABLES FOR THIS TO RUN CORRECTLY
+	ub = lb +q;             
+   int cur = lb; 
+   sum=0;
+
+   for(int p = 0; p <= q; p++){   
+      sum = sum + (lb+p);
    }
-   
-   else{
-      printf("runner skipped\n");
-   }
-   
+   //printf("cur %d ---- ub %d\n", cur, ub);
+   printf("c = %d --- sum of %d to %d = %d\n", c, cur, ub, sum);
+   c++;
+   finalSUM += sum;
+
 	pthread_exit(0);
 }
 
