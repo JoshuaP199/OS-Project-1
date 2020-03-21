f6165568579579071d6cf25816ab601d36a8f2b9
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-03-21 17:53:26 -0400

    Add files via upload

---
+++ Turned_In/PyFinal_JP.py
@@ -0,0 +1,43 @@
+import threading
+
+N = int(input("Number to use in summation: "))
+M = int(input("Number of threads to use: "))
+
+#global holder
+holder = 0
+q = (N//M)
+rem = N%M
+
+def runner(X, Y, Z):
+    global holder
+    if (i == M-1):
+        sum = 0
+        for p in range((N-lb)+1):
+            sum = sum + (lb+p)
+        print("sum of ", lb, " to ", ub, " = ", sum)
+        holder += sum
+    else:
+        sum = 0
+        for p in range(q):
+            sum = sum + (lb+p)
+        print("sum of ", lb, " to ", ub, " = ", sum)
+        holder += sum
+    #return holder
+    
+if __name__ == "__main__": 
+    for i in range(M):
+        if i == 0:
+            lb = 1
+            ub = lb + (q-1)
+        elif (i == M-1):
+            ub = N
+            lb = (ub - (q+rem)+1)
+        else:
+            lb = 1 + (q*i)
+            ub = lb + (q-1)
+        a1= threading.Thread(target=runner, args=(lb,ub,i)) 
+        a1.start()
+        a1.join() 
+        #holder += holder
+    print('Sum of 1 to ', N, ' = ', holder)
+    print("Done!") 
